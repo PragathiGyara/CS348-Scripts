2c16b021ffc70e5c697d98e3a2cefa627630d672
PragathiGyara <gyarapragathi@gmail.com>
2025-09-06 11:47:07 +0530

    Extracting questions wise answers

--- GetBoundaries.py
+++ GetBoundaries.py
@@ -7,14 +7,16 @@ output_folder = "output_images"
 os.makedirs(output_folder, exist_ok=True)
 
 pages = convert_from_path(pdf_path, dpi=300)
-
 first_page = pages[0]
 img_path = os.path.join(output_folder, "page1.png")
 first_page.save(img_path, "PNG")
 
 # Load image with OpenCV
 img = cv2.imread(img_path)
-clone = img.copy()
+
+total_height, total_width, c = img.shape  
+
+print(f"Image dimensions  Width: {total_width}px, Height: {total_height}px")
 coords = []
 
 # Mouse callback function
@@ -25,20 +27,22 @@ def click_event(event, x, y, flags, param):
         print(f"Clicked at: x={x}, y={y}")
         if len(coords) == 2:
             cv2.rectangle(img, coords[0], coords[1], (0, 255, 0), 2)
-            cv2.imshow("Select Roll Number Box", img)
+            cv2.imshow("Select Box", img)
 
-            x1, y1 = coords[0]
-            x2, y2 = coords[1]
-            w = x2 - x1
-            h = y2 - y1
-            print(f"\nUse these in your OCR script:")
-            print(f"x, y, w, h = {x1}, {y1}, {w}, {h}")
+            x1_orig = int(coords[0][0] / scale)
+            y1_orig = int(coords[0][1] / scale)
+            x2_orig = int(coords[1][0] / scale)
+            y2_orig = int(coords[1][1] / scale)
+
+            w_orig = x2_orig - x1_orig
+            h_orig = y2_orig - y1_orig
+            print(f"x, y, w, h = {x1_orig}, {y1_orig}, {w_orig}, {h_orig}")
 
 # Show image and set callback
-scale = 1
+scale = 0.2 
 img_scaled = cv2.resize(img, (0,0), fx=scale, fy=scale)
+
 cv2.imshow("Select Roll Number Box", img_scaled)
-cv2.imshow("Select Roll Number Box", img)
 cv2.setMouseCallback("Select Roll Number Box", click_event)
 
 print("Click top-left and bottom-right corners of the roll number box.")
---
+++ GetQuestionwiseAnswers.py
@@ -0,0 +1,76 @@
+import os
+import cv2
+from pdf2image import convert_from_path
+
+input_folder = "quiz1_pdfs"
+# temp_folder = "temp_images"
+output_folder = "quiz1_images"
+# os.makedirs(temp_folder,exist_ok = True)
+os.makedirs(output_folder,exist_ok = True)
+
+# # x, y, w, h = 0, 1103, 2457, 303 #Coordinates for question1
+# # x, y, w, h = 0, 1096, 2476, 350
+# #x, y, w, h = 0, 1393, 2476, 280 #Coordinates for question 2
+# x, y, w, h = 0, 1376, 2476, 300
+
+question_boundaries_page1 = {
+    1: (0, 1096, 2480, 370),
+    2: (0, 1376, 2480, 320),
+    3: (0, 1640, 2480, 726),
+    4: (0, 2266, 2480, 1240)
+}
+
+question_boundaries_page2 = {
+    5: (0, 320, 2480, 890),
+    6: (0, 1040, 2480, 1205),
+    7: (0, 2105, 2480, 365),
+    8: (15, 2350, 2480, 1156)
+}
+
+pdf_files = [f for f in os.listdir(input_folder) if f.lower().endswith('.pdf')]
+
+
+for pdf_file in pdf_files:
+    pdf_path =os.path.join(input_folder,pdf_file)
+    pages = convert_from_path(pdf_path,dpi = 300)
+    first_page = pages[0]
+    first_page_path = os.path.join(output_folder, f"{os.path.splitext(pdf_file)[0]}_page1.png")
+    first_page.save(first_page_path, "PNG")
+    print("First Page saved")
+
+    second_page = pages[1]
+    second_page_path = os.path.join(output_folder, f"{os.path.splitext(pdf_file)[0]}_page2.png")
+    second_page.save(second_page_path, "PNG")
+    print("Second Page saved")
+
+    scale = 1
+    img_cv = cv2.imread(first_page_path)
+    img_cv_scaled = cv2.resize(img_cv, (0,0), fx=scale, fy=scale)
+    
+    for q_num, (x,y,w,h) in question_boundaries_page1.items():
+
+        img_with_box = img_cv_scaled.copy() 
+
+        # cv2.rectangle(img_with_box, (x, y), (x + w, y + h), (0, 255, 0), 2)  # Green box, 2px thick
+        # cv2.imwrite(os.path.join(output_folder, f"{os.path.splitext(pdf_file)[0]}_page_with_box.png"), img_with_box)
+        
+        question_crop = img_cv_scaled[y:y+h, x:x+w]
+        cv2.imwrite(os.path.join(output_folder, f"{os.path.splitext(pdf_file)[0]}_Q{q_num}.png"), question_crop)
+        print("Saved question ", q_num)
+
+    scale = 1
+    img_cv_2 = cv2.imread(second_page_path)
+    img_cv_scaled_2 = cv2.resize(img_cv_2, (0,0), fx=scale, fy=scale)
+
+    for q_num, (x,y,w,h) in question_boundaries_page2.items():
+
+        img_with_box = img_cv_scaled_2.copy() 
+
+        # cv2.rectangle(img_with_box, (x, y), (x + w, y + h), (0, 255, 0), 2)  # Green box, 2px thick
+        # cv2.imwrite(os.path.join(output_folder, f"{os.path.splitext(pdf_file)[0]}_page_with_box.png"), img_with_box)
+        
+        question_crop = img_cv_scaled_2[y:y+h, x:x+w]
+        cv2.imwrite(os.path.join(output_folder, f"{os.path.splitext(pdf_file)[0]}_Q{q_num}.png"), question_crop)
+        print("Saved question ", q_num)
+
+
